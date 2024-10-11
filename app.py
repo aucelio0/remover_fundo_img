99c823000edc3843cc7db872c82108f144d2d001
aucelio0 <mateusaucelio06@gmail.com>
2024-10-11 16:48:08 -0300

    versão 1.0

---
+++ app.py
@@ -0,0 +1,19 @@
+from rembg import remove
+from PIL import Image
+
+while True:
+    try: 
+        imagem = input("Informe o nome da imagem com a extensão ou 'Enter para encerrar o programa: ")
+        if imagem:
+            nova_img = f'nova_{imagem}.png'
+            original = Image.open(imagem)
+            img_sem_fundo = remove(original)
+            img_sem_fundo.save(nova_img)
+            continue
+        else:
+            ...
+    except Exception as e:
+        print(f'Não foi possível remover a imagem. {e}.')
+    finally:
+        break
+    
